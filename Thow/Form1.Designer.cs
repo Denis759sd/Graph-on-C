94416c14c0ee330b4c48727377cb3550aeb130dd
Dzianis <kotickpro@gmail.com>
2020-11-27 19:17:02 +0300

    3 commit

--- Thow/Form1.Designer.cs
+++ Thow/Form1.Designer.cs
@@ -30,32 +30,19 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button_Start = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button_Stop = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.track_angle = new System.Windows.Forms.TrackBar();
             this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
             this.text_angle = new System.Windows.Forms.Label();
-            this.check_cell = new System.Windows.Forms.CheckBox();
+            this.check_grid = new System.Windows.Forms.CheckBox();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.track_angle)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button_Start
-            // 
-            this.button_Start.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.button_Start.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button_Start.Location = new System.Drawing.Point(12, 12);
-            this.button_Start.Name = "button_Start";
-            this.button_Start.Size = new System.Drawing.Size(152, 47);
-            this.button_Start.TabIndex = 1;
-            this.button_Start.Text = "Start";
-            this.button_Start.UseVisualStyleBackColor = false;
-            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
             // 
             // pictureBox1
             // 
@@ -66,33 +53,13 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
-            // button_Stop
-            // 
-            this.button_Stop.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.button_Stop.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button_Stop.Location = new System.Drawing.Point(12, 402);
-            this.button_Stop.Name = "button_Stop";
-            this.button_Stop.Size = new System.Drawing.Size(152, 47);
-            this.button_Stop.TabIndex = 4;
-            this.button_Stop.Text = "Stop";
-            this.button_Stop.UseVisualStyleBackColor = false;
-            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(30, 65);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 5;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
             // track_angle
             // 
-            this.track_angle.Location = new System.Drawing.Point(57, 91);
+            this.track_angle.Location = new System.Drawing.Point(12, 11);
             this.track_angle.Maximum = 90;
             this.track_angle.Name = "track_angle";
             this.track_angle.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.track_angle.Size = new System.Drawing.Size(45, 305);
+            this.track_angle.Size = new System.Drawing.Size(45, 437);
             this.track_angle.TabIndex = 11;
             this.track_angle.TickStyle = System.Windows.Forms.TickStyle.Both;
             this.track_angle.Scroll += new System.EventHandler(this.track_angle_Scroll);
@@ -100,36 +67,54 @@
             // text_angle
             // 
             this.text_angle.AutoSize = true;
-            this.text_angle.Location = new System.Drawing.Point(187, 11);
+            this.text_angle.Location = new System.Drawing.Point(225, 9);
             this.text_angle.Name = "text_angle";
-            this.text_angle.Size = new System.Drawing.Size(32, 13);
+            this.text_angle.Size = new System.Drawing.Size(13, 13);
             this.text_angle.TabIndex = 12;
-            this.text_angle.Text = "Угол";
+            this.text_angle.Text = "0";
             // 
-            // check_cell
+            // check_grid
             // 
-            this.check_cell.AutoSize = true;
-            this.check_cell.Location = new System.Drawing.Point(318, 7);
-            this.check_cell.Name = "check_cell";
-            this.check_cell.Size = new System.Drawing.Size(80, 17);
-            this.check_cell.TabIndex = 14;
-            this.check_cell.Text = "checkBox1";
-            this.check_cell.UseVisualStyleBackColor = true;
+            this.check_grid.AutoSize = true;
+            this.check_grid.Location = new System.Drawing.Point(84, 59);
+            this.check_grid.Name = "check_grid";
+            this.check_grid.Size = new System.Drawing.Size(45, 17);
+            this.check_grid.TabIndex = 14;
+            this.check_grid.Text = "Grid";
+            this.check_grid.UseVisualStyleBackColor = true;
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Location = new System.Drawing.Point(72, 30);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(75, 23);
+            this.button_Stop.TabIndex = 15;
+            this.button_Stop.Text = "Clear";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Угол:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.check_cell);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.check_grid);
             this.Controls.Add(this.text_angle);
             this.Controls.Add(this.track_angle);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button_Stop);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button_Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -145,15 +130,14 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button_Start;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button_Stop;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.TrackBar track_angle;
         private System.Windows.Forms.BindingSource bindingSource2;
         private System.Windows.Forms.Label text_angle;
-        private System.Windows.Forms.CheckBox check_cell;
+        private System.Windows.Forms.CheckBox check_grid;
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Thow/Form1.cs
+++ Thow/Form1.cs
@@ -13,21 +13,15 @@ namespace Thow
     public partial class Form1 : Form
     {
         double x = 0, y = 0;
-        double angle = Math.PI /180;        //угол в радианах
+        double angle = Math.PI /180;        //угол
         double al = 0;
-        double t = 2;                       //Время
+        double t = 10;                       //Время
         double V = 0;                       //Скорость
         const int g = 10;                   //Ускорение свободного радения
         int h = 0;
 
         int wX;
         int hX;
-        double xF, yF;
-        double step;
-
-        private Graphics graphics;
-
-        bool isActive = false;
 
         Pen PenFromGrafic = new Pen(Brushes.Black); 
         Pen PenFromLine = new Pen(Brushes.Black, 2f); 
@@ -35,10 +29,13 @@ namespace Thow
         public Form1()
         {
             InitializeComponent();
-            textBox1.TextChanged += textBox1_TextChanged;
             track_angle.Scroll += track_angle_Scroll;
-            
-            al = 60 * angle;
+
+            this.Paint += new PaintEventHandler(Draw);
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Enabled = true;
+            timer1.Interval = 1;
+            al = 80 * angle;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -53,17 +50,18 @@ namespace Thow
 
         private void Draw(object sender, PaintEventArgs e)
         {
-            h = (int)V * (int)V / 2 / g;
-
             wX = pictureBox1.Width;
             hX = pictureBox1.Height;
+
+            h = (int)V * (int)V / 2 / g;     
 
             x = (V * Math.Cos(al)) * t;
             y = (V * Math.Sin(al)) * t - g * t * t / 2;
 
             Bitmap flag = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             Graphics graph = Graphics.FromImage(flag);
-            if (check_cell.Checked == true)
+
+            if (check_grid.Checked == true)
             {
                 graph.DrawLine(PenFromGrafic, 0, (int)((hX / 4) - 100), wX, (int)((hX / 4) - 100));
                 graph.DrawLine(PenFromGrafic, 0, (int)(hX / 4), wX, (int)(hX / 4));
@@ -82,68 +80,45 @@ namespace Thow
                 
             
             Point start = new Point(1, 400);
-            Point control1 = new Point((int)x, (int)(h / y));
-            Point control2 = new Point((int)x, (int)(h / y));
-            Point end = new Point((int)(x +V), hX - 1);
+            Point control1 = new Point((int)(x * al), (int)(y / hX));
+            Point control2 = new Point((int)(x * al), (int)(y / hX));
+            Point end = new Point((int)(x + V / al), (int)hX - 1);
 
-            //graph.FillRectangle(Brushes.Black, new Rectangle((int)(50 * x), (int)(400 - 50 * y),5,5));
+            graph.FillRectangle(Brushes.Black, new Rectangle((int)(50 * x), (int)(400 - 50 * y),5,5));
             //graph.DrawBezier(PenFromLine, 1, 400, wX/2, hX/2, (int)(50 * x), (int)(400 - 50 * y), 0, 0);
             graph.DrawBezier(PenFromLine, start, control1, control2, end);
             pictureBox1.Image = flag;
         }
-
-        private void button_Start_Click(object sender, EventArgs e) => Start(true);
-
             
         private void button_Stop_Click(object sender, EventArgs e)
         {
             timer1.Stop();
             x = 0;
             y = 0;
-            t = 0;
-            V = 0;
-            angle = 0;
+            angle = Math.PI / 180;        
+            t = 5;                       
+            V = 0;                                        
+            h = 0;
+            text_angle.Text = "0";
+            al = 0;
             track_angle.Value = 0;
-        }
-
-        private void Start(bool active)
-        {
-            if (active == true)
-            {
-                this.Paint += new PaintEventHandler(Draw);
-                timer1.Tick += new EventHandler(timer1_Tick);
-                timer1.Enabled = true;
-                timer1.Interval = 50;
-            }
-            else
-                return;
         }
 
         private void track_angle_Scroll(object sender, EventArgs e)
         {
-            angle = track_angle.Value;
-            //sal = track_angle.Value * angle;
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            try
-            {
-                V = Convert.ToDouble(textBox1.Text);
-            }
-            catch (System.FormatException) { }
-                     
+            V = track_angle.Value;
+            al = track_angle.Value * angle;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            text_angle.Text = Math.Round(angle /al).ToString();
-
-           //t += 0.01;
-           //x = (V * Math.Cos(al)) * t;
-           //y = (V * Math.Sin(al)) * t - g * t * t/2;
-           //this.Text = x.ToString() + " " + y.ToString() + " " + "Время: " + t + "Скорость: " + V;
-           //Invalidate();
+            text_angle.Text = (al / angle).ToString();
+            
+            //t += 0.01;
+            //x = (V * Math.Cos(al)) * t;
+            //y = (V * Math.Sin(al)) * t - g * t * t/2;
+            //this.Text = x.ToString() + " " + y.ToString() + " " + "Время: " + t + "Скорость: " + V;
+            //Invalidate();
         }   
     }
 
